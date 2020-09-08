cb26f59f19378f754a7fea43dfaccbdee2f4809e
Shannon Pollard <spollard@elon.edu>
2020-09-08 15:02:35 -0400

    Fully cleaned up version of the Bins algorithm

--- src/Bins.java
+++ src/Bins.java
@@ -3,28 +3,39 @@ import java.io.*;
 
 public class Bins{
 
+	private static final String WORST_FIT_DECREASING = "worst-fit decreasing";
+	private static final String WORST_FIT = "worst-fit";
 	private static final double GIG = 1000000.0;
 	private static final String DEFAULT_FILE_NAME = "sizes.txt";
 
+	/*
+	 * Main algorithm for the program:
+	
 	// Read the data file
 	// Do the first disc experiment
 	// Print results
 	// Do second experiment
 	// Print results
-	public void runDiscExperiment() {
+	 * 
+	 * 
+	 */
+	public void runDiskExperiment() {
 		List<Integer> fileSizes = makeDataFromFile(DEFAULT_FILE_NAME);
-		PriorityQueue<Disk> disks = this.calculatedNeededDisksForFiles(fileSizes);
-		this.printDiscsCreated(disks, "worst-fit");
+		Collection<Disk> disks = this.calculatedNeededDisksForFiles(fileSizes);
+		this.printDiscsCreated(disks, WORST_FIT);
+		// Reverse the order for the second experiment
 		Collections.sort(fileSizes, Collections.reverseOrder());
 		disks = this.calculatedNeededDisksForFiles(fileSizes);
-		this.printDiscsCreated(disks, "worst-fit decreasing");		
+		this.printDiscsCreated(disks, WORST_FIT_DECREASING);		
 	}
+	
+	
 	/*
 	 * Given a filename, open the file and return
 	 * a list of integers from the file
 	 * Assumes: filename exists and has integers in it
 	 */
-	public List<Integer> makeDataFromFile(String filename){
+	private List<Integer> makeDataFromFile(String filename){
 		Scanner scannerForFile;
 		try {
 			scannerForFile = new Scanner(new File(filename));
@@ -38,123 +49,61 @@ public class Bins{
 			return data;
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
-			return null;
+			// It's better to return an empty structure than a null one.
+			return new ArrayList<Integer>();
 		}
 	}
 
-	public PriorityQueue<Disk> calculatedNeededDisksForFiles(List<Integer> files){
+	/*
+	 * Uses a greedy approach to save the given files onto disks.
+	 */
+	private Collection<Disk> calculatedNeededDisksForFiles(List<Integer> fileSizes){
 
 		PriorityQueue<Disk> pq = new PriorityQueue<Disk>();
 		pq.add(new Disk(0));
 		int diskId = 1;
-		for (Integer size : files){
+		for (Integer oneFileSize : fileSizes){
 			Disk d = pq.peek();
-			if (d.freeSpace() >= size){
+			if (d.freeSpace() >= oneFileSize){
 				pq.poll();
-				d.add(size);
+				d.add(oneFileSize);
 				pq.add(d);
 			}
 			else{
 				Disk d2 = new Disk(diskId);
 				diskId++;
-				d2.add(size);
+				d2.add(oneFileSize);
 				pq.add(d2);
 			}
 		}
 		return pq;
 	}
 
-	public void printDiscsCreated(PriorityQueue<Disk> pq, String algorithm) {
-		//System.out.println("total size = " + total / GIG + "GB");
+	/*
+	 * Prints to the console various statistics about a collection
+	 * of disks created with the given algorithm.
+	 */
+	public void printDiscsCreated(Collection<Disk> usedDisks, String algorithm) {
+		System.out.println("total size = " + totalMemoryUsed(usedDisks) / GIG + "GB");
 		System.out.println();
 		System.out.println(algorithm);
-		System.out.println("number of disks used: " + pq.size());
-		while (! pq.isEmpty()){
-			System.out.println(pq.poll());
+		System.out.println("number of disks used: " + usedDisks.size());
+		for(Disk disk: usedDisks) {
+			System.out.println(disk);
 		}
 		System.out.println();
-
 	}
 
-	/*public static void main (String args[]){
-		try{
-			Scanner input = new Scanner(new File("sizes.txt"));
-			List<Integer> data = new ArrayList<Integer>();
-
-			while (input.hasNext()){
-				data.add(input.nextInt());
-			}
-
-			PriorityQueue<Disk> pq = new PriorityQueue<Disk>();
-			pq.add(new Disk(0));
-
-			int diskId = 1;
-			int total = 0;
-			for (Integer size : data){
-				Disk d = pq.peek();
-				if (d.freeSpace() >= size){
-					pq.poll();
-					d.add(size);
-					pq.add(d);
-				}
-				else{
-					Disk d2 = new Disk(diskId);
-					diskId++;
-					d2.add(size);
-					pq.add(d2);
-				}
-				total += size;
-			}
-
-			System.out.println("total size = " + total / 1000000.0 + "GB");
-			System.out.println();
-			System.out.println("worst-fit method");
-			System.out.println("number of disks used: " + pq.size());
-			while (! pq.isEmpty()){
-				System.out.println(pq.poll());
-			}
-			System.out.println();
-
-			Collections.sort(data, Collections.reverseOrder());
-			pq.add(new Disk(0));
-
-			diskId = 1;
-			for (Integer size : data){
-				Disk d = pq.peek();
-				if (d.freeSpace() >= size){
-					pq.poll();
-					d.add(size);
-					pq.add(d);
-				}
-				else{
-					Disk d2 = new Disk(diskId);
-					diskId++;
-					d2.add(size);
-					pq.add(d2);
-				}
-			}
-			System.out.println("total size = " + total / 1000000.0 + "GB");
-			System.out.println();
-			System.out.println("worst-fit decreasing method");
-			System.out.println("number of disks used: " + pq.size());
-			while (! pq.isEmpty()){
-				System.out.println(pq.poll());
-			}
-			System.out.println();
-
-			//************
-			System.out.println("total size = " + total / 1000000.0 + "GB");
-			System.out.println();
-			System.out.println("worst-fit method");
-			System.out.println("number of disks used: " + pq.size());
-			while (! pq.isEmpty()){
-				System.out.println(pq.poll());
-			}
-			System.out.println();
+	
+	/*
+	 * Compute the total amount of used file space for a 
+	 * collection of Disks
+	 */
+	public int totalMemoryUsed(Collection<Disk> allDisks) {
+		int total = 0;
+		for(Disk disk: allDisks) {
+			total += disk.getSize();
 		}
-		catch (FileNotFoundException e){
-			System.err.println("Could not open file: " + args[0]);
-		}
+		return total;
 	}
-	*/
 }
--- src/Disk.java
+++ src/Disk.java
@@ -10,7 +10,8 @@ import java.util.*;
  *  files: a list of file sizes.  Totaling the file sizes should give the disk size
  */
 public class Disk implements Comparable<Disk>{
-    private int myId;
+    private static final int DEFAULT_START_SIZE = 1000000;
+	private int myId;
     private int mySize;
     private int myCapacity;
     private List<Integer> myFiles;
@@ -18,18 +19,21 @@ public class Disk implements Comparable<Disk>{
 
     public Disk(){
         mySize = 0;
-        myCapacity = 1000000;
+        myCapacity = DEFAULT_START_SIZE;
         myFiles = new ArrayList<Integer>();
     }
 
     public Disk (int id){
-    	mySize = 0;
-        myCapacity = 1000000;
-        myFiles = new ArrayList<Integer>();
+    	this();
         myId = id;
     }
+    
+    // returns amount of space used on disk
+    public int getSize() {
+    	return mySize;
+    }
 
-    // returns amount of free space available on disk
+    // returns amount of unused memory available on disk
     public int freeSpace (){
         return myCapacity - mySize;
     }
@@ -48,11 +52,17 @@ public class Disk implements Comparable<Disk>{
         return result;
     }
 
+    /*
+     * Only standard of equivalency right now is the ID number
+     */
     public boolean equals(Disk other){
-        if (other != null) return myId == other.myId;
-        else  return false;
+        return other!= null && myId == other.myId;
     }
 
+    /*
+     * Compares based on amount of free space, then uses id number to 
+     * break ties.
+     */
     public int compareTo (Disk other){
         if (other != null){
             int result = other.freeSpace() - freeSpace();
